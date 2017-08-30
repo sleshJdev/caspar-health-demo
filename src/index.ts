3e0a29feb5f1163c75d34993238568b62cad994c
Yauheni Putsykovich <slesh.eugene93@gmail.com>
2017-08-30 10:36:37 +0300

    removed comments, added bootstrap to dependency

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,7 @@ require('./bower_components/bootstrap/dist/css/bootstrap.min.css');
 import {AppModule} from './app/app.module';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .then(() => console.log("App is starter"))
-    .catch((e) => console.log("App is failed", e));
+platformBrowserDynamic().bootstrapModule(AppModule);
 
 
 
